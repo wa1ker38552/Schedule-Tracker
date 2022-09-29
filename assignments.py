da3138d037b28261a5b239bc353099f866ab22d5
walker <100868154+wa1ker38552@users.noreply.github.com>
2022-09-29 16:23:54 -0700

    fetch assignments

---
+++ assignments.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from pytz import timezone  
+import requests
+
+def get_assignments(cookie):
+  headers = {'cookie': cookie}
+  # get date
+  today = datetime.today().astimezone(timezone('US/Pacific')).strftime("%Y-%m-%d")
+  request = requests.get(f'https://mvla.instructure.com/api/v1/planner/items?start_date={today}T07%3A00%3A00.000', headers=headers)
+
+  assignments = []
+  for i, assignment in enumerate(request.json()):
+    assignments.append({
+      'title': assignment['plannable']['title']
+    })
+    try:
+      assignments[i]['due'] = assignment['plannable']['due_at'].split('T')[0]
+    except KeyError: assignments[i]['due'] = None
+    except AttributeError: assignments[i]['due'] = None
+
+  return assignments
